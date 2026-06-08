04fdc1a7519a4c09602ef1f5e49c60047d23f24c
yigabriel <gabrielsacc7@gmail.com>
2026-06-07 22:39:30 -0500

    hotfix: SecurityConfiguration for auth

--- src/main/java/com/upc/pe/backend/shared/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/upc/pe/backend/shared/infrastructure/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -15,20 +16,19 @@ public class SecurityConfiguration {
     private static final String SWAGGER_UI = "/swagger-ui/**";
     private static final String SWAGGER_HTML = "/swagger-ui.html";
     private static final String OPEN_API = "/v3/api-docs/**";
+    private static final String AUTH = "/api/v1/authentication/**";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/api/v1/authentication/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/v3/api-docs/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                );
+        http.authorizeHttpRequests(auth -> auth.requestMatchers(
+                ROOT,
+                ERROR,
+                SWAGGER_UI,
+                SWAGGER_HTML,
+                OPEN_API,
+                AUTH
+        ).permitAll().anyRequest().authenticated());
+
         return http.build();
     }
 }
